eec7f06c3b737307ba8113dcd54e28787c015f14
Blackflag <Blackflag88@yandex.ru>
2023-01-30 18:28:59 +0400

    Решили задчу 8

---
+++ HomeWork_Task8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int startNumber = 1;
+
+
+while(startNumber <= number)
+    {
+        if(startNumber % 2 ==0)
+        Console.Write(startNumber + ", ");
+        startNumber ++;
+    }
