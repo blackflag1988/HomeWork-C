a83c675d3cdd385632621b2ed1e45e8c9be76cfd
Blackflag <Blackflag88@yandex.ru>
2023-02-19 11:10:31 +0400

    Решили задчу 41

---
+++ HomeWork_Task41/Program.cs
@@ -0,0 +1,36 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+Console.WriteLine("Введите последовательно числа");
+Console.WriteLine("Какое количество цифр вы хотите ввести?");
+int m = int.Parse(Console.ReadLine()!);
+
+int[] GetArray(int m)
+{
+    int[] array = new int[m];
+    for (int i = 0; i < m; i++)
+    {
+        Console.WriteLine($"Введите число № {i + 1}");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+int AfterZero(int[] array)
+{
+    int result = 0;
+    foreach (var item in array)
+    {
+        if(item > 0)
+        {
+            result += 1;
+        }
+    }
+    return result;
+}
+
+int[] newArray = GetArray(m);
+Console.WriteLine(String.Join(", ", newArray));
+
+int afterZero = AfterZero(newArray);
+Console.WriteLine($"Из введённого количества {afterZero} цифр - больше 0!");
