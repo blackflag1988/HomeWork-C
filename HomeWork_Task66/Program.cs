adb2ee665105663cd4f03ab353c46d3e3add4e47
Blackflag <Blackflag88@yandex.ru>
2023-03-01 18:32:29 +0400

    Решили задчу 66

---
+++ HomeWork_Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+
+int Recursiv(int N, int M)
+{
+    int sum = 0;
+    if (N == M) return N;
+    else
+    {
+        return sum = N + Recursiv(N + 1, M);
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите число от: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число до: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+Console.WriteLine(Recursiv(N, M));
